94bb9ec8815828d5d434eae9047078aedc26e0f0
Saswat Samal <saswat1091samal@gmail.com>
2026-04-24 14:00:17 +0530

    add cryption logic

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -1,6 +1,9 @@
 #include "ProcessManagement.hpp"
 #include <iostream>
 #include <cstring>
+#include <memory>
+#include <sys/wait.h>
+#include "../encryptDecrypt/Cryption.hpp"
 
 ProcessManagement::ProcessManagement() {}
 
@@ -12,5 +15,10 @@ bool ProcessManagement::sumbitToQueue(std::unique_ptr<Task> task)
 
 void ProcessManagement::executeTask() 
 {
-    
+   while(!taskQueue.empty()) {
+	   std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
+	   taskQueue.pop();
+	   std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
+	   executeCryption(taskToExecute->toString());
+   }
 }
